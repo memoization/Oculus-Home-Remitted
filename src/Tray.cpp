e30d3f854691fa3a80f869c1bb1fbb1cd0baa4a6
memoization <50002278+memoization@users.noreply.github.com>
2026-09-15 13:36:26 -0400

    Fix systray icon disappearance & launch option in the system tray menu

--- src/Tray.cpp
+++ src/Tray.cpp
@@ -7,12 +7,14 @@ static const UINT WM_TRAY = WM_APP + 1;
 enum
 {
     ID_TRAY_RESTORE = 1001,
-    ID_TRAY_QUIT = 1002
+    ID_TRAY_QUIT = 1002,
+    ID_TRAY_LAUNCH = 1003,
 };
 
 static WNDPROC g_origProc = nullptr;
 static NOTIFYICONDATAW g_nid = {};
 static HWND g_hwnd = nullptr;
+static UINT WM_TASKBARCREATED = 0;
 
 static void RestoreWindow(HWND h)
 {
@@ -27,6 +29,7 @@ static void ShowTrayMenu(HWND h)
     POINT pt;
     GetCursorPos(&pt);
     HMENU menu = CreatePopupMenu();
+    AppendMenuW(menu, MF_STRING, ID_TRAY_LAUNCH, L"Launch Oculus Home");
     AppendMenuW(menu, MF_STRING, ID_TRAY_RESTORE, L"Open Oculus Home Remitted");
     AppendMenuW(menu, MF_SEPARATOR, 0, nullptr);
     AppendMenuW(menu, MF_STRING, ID_TRAY_QUIT, L"Quit");
@@ -35,8 +38,18 @@ static void ShowTrayMenu(HWND h)
     DestroyMenu(menu);
 }
 
+static void AddTrayIcon()
+{
+    Shell_NotifyIconW(NIM_ADD, &g_nid);
+}
+
 static LRESULT CALLBACK TrayProc(HWND h, UINT msg, WPARAM w, LPARAM l)
 {
+    if (msg == WM_TASKBARCREATED)
+    {
+        AddTrayIcon();
+        return 0;
+    }
     if (msg == WM_TRAY)
     {
         if (l == WM_LBUTTONUP || l == WM_LBUTTONDBLCLK)
@@ -57,6 +70,11 @@ static LRESULT CALLBACK TrayProc(HWND h, UINT msg, WPARAM w, LPARAM l)
             ui.keepAlive.store(false); // render loop exits, app quits (Remove() runs on the way out)
             return 0;
         }
+        if (LOWORD(w) == ID_TRAY_LAUNCH)
+        {
+            ui.DoLaunchHome();
+            return 0;
+        }
     }
 
     return CallWindowProcW(g_origProc, h, msg, w, l);
@@ -65,42 +83,45 @@ static LRESULT CALLBACK TrayProc(HWND h, UINT msg, WPARAM w, LPARAM l)
 namespace tray
 {
 
-void Install(HWND hwnd)
-{
-    if (!hwnd || g_hwnd)
-        return;
-    g_hwnd = hwnd;
-    g_origProc = reinterpret_cast<WNDPROC>(SetWindowLongPtrW(hwnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(TrayProc)));
-
-    HICON icon = LoadIconW(GetModuleHandleW(nullptr), L"GLFW_ICON");
-    if (!icon)
+    void Install(HWND hwnd)
     {
-        icon = reinterpret_cast<HICON>(SendMessageW(hwnd, WM_GETICON, ICON_SMALL, 0));
-        icon = LoadIconW(nullptr, IDI_APPLICATION);
+        if (!hwnd || g_hwnd) return;
+
+        g_hwnd = hwnd;
+        g_origProc = reinterpret_cast<WNDPROC>(SetWindowLongPtrW(hwnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(TrayProc)));
+
+        HICON icon = LoadIconW(GetModuleHandleW(nullptr), L"GLFW_ICON");
+        if (!icon)
+        {
+            icon = reinterpret_cast<HICON>(SendMessageW(hwnd, WM_GETICON, ICON_SMALL, 0));
+            icon = LoadIconW(nullptr, IDI_APPLICATION);
+        }
+
+        g_nid = {};
+        g_nid.cbSize = sizeof(g_nid);
+        g_nid.hWnd = hwnd;
+        g_nid.uID = 1;
+        g_nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+        g_nid.uCallbackMessage = WM_TRAY;
+        g_nid.hIcon = icon;
+        wcscpy_s(g_nid.szTip, L"Oculus Home Remitted");
+        AddTrayIcon();
+
+        // Listener to recreate in case explorer crashes
+        WM_TASKBARCREATED = RegisterWindowMessageW(L"TaskbarCreated");
     }
 
-    g_nid = {};
-    g_nid.cbSize = sizeof(g_nid);
-    g_nid.hWnd = hwnd;
-    g_nid.uID = 1;
-    g_nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
-    g_nid.uCallbackMessage = WM_TRAY;
-    g_nid.hIcon = icon;
-    wcscpy_s(g_nid.szTip, L"Oculus Home Remitted");
-    Shell_NotifyIconW(NIM_ADD, &g_nid);
-}
-
-void Remove()
-{
-    if (!g_hwnd) return;
-    Shell_NotifyIconW(NIM_DELETE, &g_nid);
-    if (g_origProc)
+    void Remove()
     {
-        SetWindowLongPtrW(g_hwnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(g_origProc));
-    }
+        if (!g_hwnd) return;
+        Shell_NotifyIconW(NIM_DELETE, &g_nid);
+        if (g_origProc)
+        {
+            SetWindowLongPtrW(g_hwnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(g_origProc));
+        }
         
-    g_hwnd = nullptr;
-    g_origProc = nullptr;
-}
+        g_hwnd = nullptr;
+        g_origProc = nullptr;
+    }
 
 }
